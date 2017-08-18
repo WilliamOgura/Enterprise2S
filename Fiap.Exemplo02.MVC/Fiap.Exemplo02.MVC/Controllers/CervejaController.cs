bb88cdc5e3d342847cc303db8610ee318c15a26b
WilliamOgura <william.ogura98@gmail.com>
2017-08-18 11:29:11 -0300

    Cerveja Implementada cadastro+Listagem

---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Controllers/CervejaController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Exemplo02.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Controllers
+{
+    public class CervejaController : Controller
+    {
+        private static List<Cerveja> _banco = new List<Cerveja>();
+        
+        [HttpGet]    
+        public ActionResult Cadastrar()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Cerveja cerveja)
+        {
+            TempData["msg"] = "Cadastrado com sucesso";
+            _banco.Add(cerveja);
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            
+            return View(_banco);
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Models/Cerveja.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo02.MVC.Models
+{
+    public class Cerveja
+    {
+        [DisplayName("Nome")]
+        public string Nome { get; set; }
+        
+        public string Marca { get; set; }
+
+        [DisplayName("Preço")]
+        [UIHint("Decimal")]
+        public decimal Preco { get; set; }
+        [DisplayName("Possui álcool?")]
+        public bool SemAlcool { get; set; }
+        [DisplayName("Tipo de cerveja")]
+        public string Tipo { get; set; }
+        [UIHint("Date")]
+        public DateTime Validade { get; set; }
+
+    }
+}
